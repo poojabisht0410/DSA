74cfcb71fac245ed267648173a6e4b2d03281b85
Pooja-Bisht <89833706+poojabisht0410@users.noreply.github.com>
2022-01-11 18:49:18 +0530

    Add files via upload

---
+++ pattern_questions/mirrored_right_triangle_starpattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+    int N;
+    cin>>N;
+
+
+    for(int i=0;i<N;i++){
+
+        for(int j=N-1-i;j>0;j--){
+            cout<<"   ";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<" * ";
+        }
+
+        cout<<endl;
+    }
+return 0;
+}
