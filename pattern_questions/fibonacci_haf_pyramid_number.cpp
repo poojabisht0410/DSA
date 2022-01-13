dbf2d3d1e771b012557019786ec512589367aeeb
Pooja-Bisht <89833706+poojabisht0410@users.noreply.github.com>
2022-01-13 19:52:19 +0530

    Add files via upload

---
+++ pattern_questions/fibonacci_haf_pyramid_number.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+    int N;
+    cin>>N;
+int fib=1;
+int k=0;
+int l=1;
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=i;j++){
+
+            cout<<fib<<" ";
+            fib=k+l;
+            k=l;
+            l=fib;
+
+            
+        }
+        cout<<endl;
+    }
+return 0;
+}
