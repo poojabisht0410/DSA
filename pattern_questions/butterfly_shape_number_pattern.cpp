51aa49020601c101910c13d7f47a6244b3711634
Pooja-Bisht <89833706+poojabisht0410@users.noreply.github.com>
2022-01-14 11:18:32 +0530

    Add files via upload

---
+++ pattern_questions/butterfly_shape_number_pattern.cpp
@@ -0,0 +1,39 @@
+
+
+#include<iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+    int N;
+    cin>>N;
+    int x=1;
+
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+
+            if(j<=x || j>=(N-x)+1 ){
+                cout<<j;
+            }
+            else{
+                cout<<" ";
+            }
+        }
+
+        if(i<=N/2){
+            x++;
+        }
+        else{
+            x--;
+        }
+
+        cout<<endl;
+        
+        
+    }
+
+return 0;
+}
